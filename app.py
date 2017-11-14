c2258a2b9428df4a04e0e07a6b0abe72876c7a9a
rdv_chio <rdv.chio@gmail.com>
2017-11-14 01:07:54 -0600

    Fool around another tutorial

--- app.py
+++ app.py
@@ -1,26 +1,15 @@
-from flask import Flask, render_template, request
-import pandas as pd
-#from bokeh.charts import Histogram
-#from bokeh.embed import components
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-# Load the Iris Data Set
-iris_df = pd.read_csv("data/data_1.txt", 
-    names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
-feature_names = iris_df.columns[0:-1].values.tolist()
+
+@app.route("/<int:bars_count>/")
+def chart(bars_count):
+    if bars_count <= 0:
+        bars_count = 1
+    return render_template("chart.html", bars_count=bars_count)
 
 
-# Index page
-@app.route('/')
-def index():
-	# Determine the selected feature
-	current_feature_name = request.args.get("feature_name")
-	if current_feature_name == None:
-		current_feature_name = "Sepal Length"
-
-
-# With debug=True, Flask server will auto-reload 
-# when there are code changes
-if __name__ == '__main__':
-	app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
