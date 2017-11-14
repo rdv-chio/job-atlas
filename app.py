2ea5ed64ef82b04192068525e04217f974ff8fa1
rdv_chio <rdv.chio@gmail.com>
2017-11-14 00:48:27 -0600

    Fool around remove plot

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from bokeh.charts import Histogram
-from bokeh.embed import components
+#from bokeh.charts import Histogram
+#from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -10,17 +10,6 @@ iris_df = pd.read_csv("data/data_1.txt",
     names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
 feature_names = iris_df.columns[0:-1].values.tolist()
 
-# Create the main plot
-#def create_figure(current_feature_name, bins):
-#	p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species', 
-#	 	bins=bins, legend='top_right', width=600, height=400)
-
-	# Set the x axis label
-#	p.xaxis.axis_label = current_feature_name
-
-	# Set the y axis label
-#	p.yaxis.axis_label = 'Count'
-#	return p
 
 # Index page
 @app.route('/')
@@ -30,13 +19,6 @@ def index():
 	if current_feature_name == None:
 		current_feature_name = "Sepal Length"
 
-	# Create the plot
-#	plot = create_figure(current_feature_name, 10)
-		
-	# Embed plot into HTML via Flask Render
-#	script, div = components(plot)
-#	return render_template("index.html", script=script, div=div,
-#		feature_names=feature_names,  current_feature_name=current_feature_name)
 
 # With debug=True, Flask server will auto-reload 
 # when there are code changes
