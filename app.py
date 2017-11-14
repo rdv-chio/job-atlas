9c35560fffa71944e2e6fa37022c5bbe55aa7b38
rdv_chio <rdv.chio@gmail.com>
2017-11-14 07:26:21 -0600

    back to Hello Flask

--- app.py
+++ app.py
@@ -1,43 +1,15 @@
+# Hello, Flask!
 from flask import Flask, render_template, request
-import pandas as pd
-from bokeh.charts import Histogram
-from bokeh.embed import components
-#import holoviews as 
 
 app = Flask(__name__)
 
-# Load the Iris Data Set
-iris_df = pd.read_csv("data_1.txt", 
-    names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
-feature_names = iris_df.columns[0:-1].values.tolist()
-
-# Create the main plot
-def create_figure(current_feature_name, bins):
-	p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species', 
-	 	bins=bins, legend='top_right', width=600, height=400)
-
-	# Set the x axis label
-	p.xaxis.axis_label = current_feature_name
-
-	# Set the y axis label
-	p.yaxis.axis_label = 'Count'
-	return p
-
-# Index page
+# Index page, no args
 @app.route('/')
 def index():
-	# Determine the selected feature
-	current_feature_name = request.args.get("feature_name")
-	if current_feature_name == None:
-		current_feature_name = "Sepal Length"
-
-	# Create the plot
-	plot = create_figure(current_feature_name, 10)
-		
-	# Embed plot into HTML via Flask Render
-	script, div = components(plot)
-	return render_template("index.html", script=script, div=div,
-		feature_names=feature_names,  current_feature_name=current_feature_name)
+	name = request.args.get("name")
+	if name == None:
+		name = "Edward"
+	return render_template("index.html", name=name)
 
 # With debug=True, Flask server will auto-reload 
 # when there are code changes
